bd4388ae81977ee13bf2bf77dde348d091ce56f5
LEVYABDI <kevyabdi@gmail.com>
2025-08-02 14:08:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "8271624089:AAGPR01siAHqOYGMoc-x2f1yC7ToqG8HQKU")
+TOKEN = os.environ.get("TOKEN", "8271624089:AAGenBYTfLSt_zO046z_jwKXd0HYynKbg5Y")
 
 API_ID = int(os.environ.get("API_ID", "26176218"))
 
